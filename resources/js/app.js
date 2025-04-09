768461dcecc12f9790c4ebb548b365e729dbbe66
luizalvesot <alvesluizotavio23@gmail.com>
2025-04-08 21:09:52 -0300

    adicionado código para trabalhar as máscaras dos campos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,18 @@
-import './bootstrap';
+//import './bootstrap';
 import 'bootstrap';
+
+import $ from 'jquery';
+import 'jquery-mask-plugin';
+
+document.addEventListener('livewire:load', function () {
+    applyMask();
+
+    // Reaplica máscara após qualquer atualização do Livewire
+    Livewire.hook('message.processed', () => {
+        applyMask();
+    });
+
+    function applyMask() {
+        $('.money').mask('#.##0,00', { reverse: true });
+    }
+});
