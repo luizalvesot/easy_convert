b297df1b90812db60e1a923cd70e712f2c2c716d
luizalvesot <alvesluizotavio23@gmail.com>
2025-04-08 21:17:32 -0300

    view criada para mostrar fechamentos dos ultimos 7 dias das moedas

---
+++ app/Livewire/Awesome/Fechamentos/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Awesome\Fechamentos;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.awesome.fechamentos.index');
+    }
+}
