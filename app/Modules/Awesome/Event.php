67190cb01044f075a8f31522bf10a5cccad92539
luizalvesot <alvesluizotavio23@gmail.com>
2025-04-08 21:19:18 -0300

    eventos implementados para acessar os endpoints da API Awesome

--- app/Modules/Awesome/Event.php
+++ app/Modules/Awesome/Event.php
@@ -11,10 +11,19 @@ class Event extends Awesome
         parent::__construct();
     }
 
-    public function retornaMoedas($data)
+    public function retornaMoedas($moedas)
     {
         //$path = 'send-code';
         $path = 'last';
+
+        return $this->request($path, 'GET', $moedas);
+    }
+
+    public function retornaFechamentoDias($moeda, $numero_dias)
+    {
+        //$path = 'send-code';
+        $path = 'json/daily';
+        $data = $moeda . "/" . $numero_dias;
 
         return $this->request($path, 'GET', $data);
     }
