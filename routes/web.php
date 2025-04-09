195ce37e3bdef17b36b62e844bed46eb8c2534c5
luizalvesot <alvesluizotavio23@gmail.com>
2025-04-08 21:13:10 -0300

    rotas criadas para acessar os métodos das moedas

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Awesome\AwesomeController;
+use App\Http\Controllers\ConversorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('retorna-moedas/{moedas}', [AwesomeController::class, 'retornaMoedas'])->name('retornaMoedas');
+Route::get('index',[ConversorController::class, 'index'])->name('index');
+
+Route::get('moedas/{moedas}', [AwesomeController::class, 'retornaMoedas'])->name('retornaMoedas');
+Route::get('fechamento-ultimos-dias/{moeda}/{numero_dias}', [AwesomeController::class, 'retornaFechamentoDias'])->name('retornaFechamentoDias');
 
 Route::get('/', function () {
     return view('welcome');
